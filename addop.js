76d6c4810ed9ff36e5d6f3ea73ec1781a4d2c991
Joseph <josephaj8055@gmail.com>
2020-12-01 15:47:23 +0530

    first commit

---
+++ addop.js
@@ -0,0 +1,11 @@
+var num1=10;
+var num2=8;
+
+
+console.log(num1+num2);
+console.log(num1-num2);
+console.log(num1*num2);
+console.log(num1/num2);
+console.log(num1%num2);
+console.log(++num1);
+console.log(--num2);
